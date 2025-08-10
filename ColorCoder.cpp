555b26eb62b36f64e358e011365303f8e8e7ff96
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-11 00:18:32 +0530

    Create ColorCoder.cpp

---
+++ ColorCoder.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+#include "ColorCoder.hpp"
+
+namespace TelCoColorCoder
+{
+    ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = static_cast<MajorColor>(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor = static_cast<MinorColor>(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
+}
