e0d33113c1a8a6d7acb6341a9ef84818971c5abe
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-11 00:20:24 +0530

    Create ColorCoderPrint.cpp

---
+++ ColorCoderPrint.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+#include "ColorCoder.hpp"
+
+namespace TelCoColorCoder
+{
+    void PrintColorCodeManual() {
+        std::cout << "Color Code Reference Manual:\n";
+        std::cout << "-----------------------------\n";
+        for (int i = 1; i <= numberOfMajorColors * numberOfMinorColors; ++i) {
+            ColorPair colorPair = GetColorFromPairNumber(i);
+            std::cout << i << " : " << colorPair.ToString() << "\n";
+        }
+    }
+}
