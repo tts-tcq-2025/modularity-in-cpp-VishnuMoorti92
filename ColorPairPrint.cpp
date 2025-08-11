8a54b73540b3892cb0823745ad5a0a80ad5fd5a7
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-11 23:54:49 +0530

    Update ColorPairPrint.cpp

--- ColorPairPrint.cpp
+++ ColorPairPrint.cpp
@@ -2,12 +2,16 @@
 
 namespace TelCoColorCoder
 {
-    void PrintColorCodeManual() {
-        std::cout << "Color Code Reference Manual:\n";
-        std::cout << "-----------------------------\n";
+    void PrintColorCodeManual(std::function<void(std::string&)> prntline) {
+        std::string Heading = "Color Code Reference Manual:\n";
+        prntline(Heading);
+        std::string Seperator = "-----------------------------\n";
+        prntline(Seperator);
+        std::string colorpair;
         for (int i = 1; i <= numberOfMajorColors * numberOfMinorColors; ++i) {
             ColorPair colorPair = GetColorFromPairNumber(i);
-            std::cout << i << " : " << colorPair.ToString() << "\n";
+            colorpair = i + " : " + colorPair.ToString() + "\n";
+            prntline(colorpair);
         }
     }
 }
