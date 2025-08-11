0516b0945a000db3f3aed8253395360a0770e1c7
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-11 22:55:08 +0530

    Create GetColorPair.hpp

---
+++ GetColorPair.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+#include "Update ColorCoder.hpp"
+
+namespace TelCoColorCoder
+{
+    ColorPair GetColorFromPairNumber(int pairNumber);
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+
+    ColorPair GetColorFromPairNumber(int pairNumber);
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+    void PrintColorCodeManual();
+}
