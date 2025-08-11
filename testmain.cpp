25360151845dcb922a8d018314f4ab581bb8f609
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-12 00:17:09 +0530

    Update testmain.cpp

--- testmain.cpp
+++ testmain.cpp
@@ -1,13 +1,22 @@
 #include "testmain.hpp"
+#include <vector>
+#include <string>
+#include <functional>
 
 int main() {
+    std::vector<std::string> messages;
+    auto printFunction = &messages {
+        messages.push_back(msg);
+    };
+
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
-    TelCoColorCoder::PrintColorCodeManual(std::function<void(std::string&)> prntline);
+    TelCoColorCoder::PrintColorCodeManual(printFunction);
 
     return 0;
 }
+
