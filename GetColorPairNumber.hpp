bd2d200a6dca70424abe980e88d568cf2811ca63
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-12 10:10:04 +0530

    Update GetColorPairNumber.hpp

--- GetColorPairNumber.hpp
+++ GetColorPairNumber.hpp
@@ -2,11 +2,10 @@
 #include <string>
 #include <cassert>
 #include "ColorPairCoder.hpp"
-#include "ContentPrint.hpp"
 
 namespace TelCoColorCoder
 {
     ColorPair GetColorFromPairNumber(int pairNumber);
     int GetPairNumberFromColor(MajorColor major, MinorColor minor);
-    void PrintColorCodeManual(std::function<void(std::string&)> prntline);
+    void PrintColorCodeManual();
 }
