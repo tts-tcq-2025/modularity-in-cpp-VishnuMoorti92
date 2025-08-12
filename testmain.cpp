aece1cea10809a5d183f9de92d639468884d0d86
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-08-12 10:09:25 +0530

    Update testmain.cpp

--- testmain.cpp
+++ testmain.cpp
@@ -17,7 +17,7 @@ int main() {
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
-    TelCoColorCoder::PrintColorCodeManual(printFunction);
+    TelCoColorCoder::PrintColorCodeManual();
     return 0;
 }
 
